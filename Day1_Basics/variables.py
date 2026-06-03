ace76c60fcdfde0d4a63478b3078013bc9067ee9
Rudra2986 <rudrapatel2156@gmail.com>
2026-06-03 20:01:01 +0530

    Added Day 1 Python practice files

---
+++ Day1_Basics/variables.py
@@ -0,0 +1,13 @@
+name = "Rudra"
+age = 20
+height = 5.8
+is_student = True
+City = 'Surat'
+Fav_Colour = 'Blue'
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(City)
+print(Fav_Colour)
