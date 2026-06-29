c5feec85fc0940fa7ee5d88ae5e20c76cb3b30bc
Rudra2986 <rudrapatel2156@gmail.com>
2026-06-29 10:59:03 +0530

    Some Dictionary Problems

--- Day7_Dictionary/problems_part1.py
+++ Day7_Dictionary/problems_part1.py
@@ -1,11 +1,9 @@
 # Day 7: Dictionary Practice - Part 1
 
 # Problem 1: Merge two dictionaries into one.
-print("--- Problem 1: Merge Dictionaries ---")
 dict1 = {'a': 10, 'b': 20}
 dict2 = {'b': 30, 'c': 40}
 
-# In Python 3.9+, we can use the | operator to merge
 merged_dict = dict1 | dict2
 print("Dict 1:", dict1)
 print("Dict 2:", dict2)
@@ -14,7 +12,6 @@ print()
 
 
 # Problem 2: Find the sum of all numeric values in a dictionary.
-print("--- Problem 2: Sum of Values ---")
 scores = {
     'math': 85,
     'science': 90,
@@ -28,7 +25,6 @@ print()
 
 
 # Problem 3: Check if a key exists in a dictionary, print its value, and insert a default if missing.
-print("--- Problem 3: Check Key and Update ---")
 inventory = {
     'apples': 10,
     'bananas': 5,
